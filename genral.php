b13d6ebb600e193b713c7537c0a9a4e34d2d46f8
dpl <dpl@narola.email>
2021-07-13 15:36:52 +0530

    improvement changes

--- genral.php
+++ genral.php
@@ -1,12 +1,15 @@
 <?php
+define('API_URL', 'https://app.iformbuilder.com/exzact/api/');
+define('VERSION', 'v60');
+define('PROFILE_ID', '502813');
+define('PAGE_ID', '3838090');
+
 /**
  * @name generate_new_token
  * @return json array
  * @author DPL
  */
 function generate_new_token() {
-
-    $curl = curl_init();
     $url = 'https://app.iformbuilder.com/exzact/api/oauth/token';
     $iat = time();
     $exp = time() + 300;
@@ -22,16 +25,34 @@ function generate_new_token() {
     ];
     $payload = json_encode($payload_arr);
     $base64UrlPayload = base64_encode($payload);
-
     $secret = '4f1d71d6b32bd592fe86a19533ab8c7bb5d678c5';
-
     $sig = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
     $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($sig));
-
     $assertion = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-
-    curl_setopt_array($curl,
-            array(
+    $param_arr = array(
+                        CURLOPT_HTTPHEADER => array(
+                            'Content-Type: application/x-www-form-urlencoded',
+                            'Cookie: PHPSESSID=6so4jr4slq0t87cck4uo74evd1; X-Mapping-fjhppofk=DA61AAF658741B5A573C938E93162983'
+                        ),
+                        CURLOPT_POST => true,
+                        CURLOPT_POSTFIELDS => 'assertion=' . $assertion . '&grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer'
+                    );
+    $response =api_call($url,'POST',$param_arr);
+    if (!empty($response)) {
+        if (isset($response['access_token'])) {
+            return $response['access_token'];
+        }
+    }
+    return false;
+}
+/**
+ * @name generate_new_token
+ * @return json array
+ * @author DPL
+ */
+function api_call($url,$method,$param_arr){
+    $curl = curl_init();
+    $curl_arr=array(
                 CURLOPT_URL => $url,
                 CURLOPT_RETURNTRANSFER => TRUE,
                 CURLOPT_SSL_VERIFYPEER => FALSE,
@@ -40,23 +61,10 @@ function generate_new_token() {
                 CURLOPT_TIMEOUT => 0,
                 CURLOPT_FOLLOWLOCATION => TRUE,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_HTTPHEADER => array(
-                    'Content-Type: application/x-www-form-urlencoded',
-                    'Cookie: PHPSESSID=6so4jr4slq0t87cck4uo74evd1; X-Mapping-fjhppofk=DA61AAF658741B5A573C938E93162983'
-                ),
-                CURLOPT_POST => true,
-                CURLOPT_POSTFIELDS => 'assertion=' . $assertion . '&grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer',
-            )
-    );
-
+                CURLOPT_CUSTOMREQUEST => $method                
+            );
+    curl_setopt_array($curl, $curl_arr+$param_arr );
     $response = curl_exec($curl);
     curl_close($curl);
-    $response = json_decode($response);
-    if (!empty($response)) {
-        if (isset($response->access_token)) {
-            return $response->access_token;
-        }
-    }
-    return false;
+    return json_decode($response, true);
 }?>
--- services.php
+++ services.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @uses This file is managing for iform builder apis
  * @author DPL
@@ -9,76 +8,34 @@ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != ''){
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 }
-
 include 'genral.php';
+$token=generate_new_token();
 if (isset($_GET['type'])){
-    /**
-     * @name retrieve_list_of_records
-     * @uses This method is using for retriving records
-     * @return json array
-     * @author DPL
-     */
+    // This method is using for retriving records
     if ($_GET['type'] == 'retrieve_list_of_records') {
         try {
-            $curl = curl_init();
-            $url = 'https://app.iformbuilder.com/exzact/api/v60/profiles/502813/pages/3838090/records?fields=company_name,category,register_date,annual_trunover&limit=100&offset=0&subform_order=desc';
-            $data = array();
-            $token = generate_new_token();
-            curl_setopt_array($curl,
-                    array(
-                        CURLOPT_URL => $url,
-                        CURLOPT_RETURNTRANSFER => TRUE,
-                        CURLOPT_SSL_VERIFYPEER => FALSE,
-                        CURLOPT_ENCODING => '',
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 0,
-                        CURLOPT_FOLLOWLOCATION => TRUE,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => 'GET',
+            $url = API_URL.''.VERSION.'/profiles/'.PROFILE_ID.'/pages/'.PAGE_ID.'/records?fields=company_name,category,register_date,annual_trunover&limit=100&offset=0&subform_order=desc';
+            $param_arr=array(
                         CURLOPT_HTTPHEADER => array('Authorization: Bearer ' . $token)
-                    )
-            );        
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $data['response'] = json_decode($response, true);
-            echo json_encode($data, true);
+                    );
+             $data['response']= api_call($url,'GET',$param_arr);
+             echo json_encode($data, true);
         } catch (Exception $e) {
             print_r($e->getMessage());
         }
     }
-    /**
-     * @name add_new_records
-     * @uses This method is using for adding records
-     * @return json array
-     * @author DPL
-     */ 
+    //This method is using for adding records
     else if ($_GET['type'] == 'add_new_records') {
         $request_body = file_get_contents('php://input');
 
         try {
-            $curl = curl_init();
-            $url = 'https://app.iformbuilder.com/exzact/api/v60/profiles/self/pages/3838090/records';
-            $token = generate_new_token();
-            $data = array();
-            curl_setopt_array($curl,
-                    array(
-                        CURLOPT_URL => $url,
-                        CURLOPT_RETURNTRANSFER => TRUE,
-                        CURLOPT_SSL_VERIFYPEER => FALSE,
-                        CURLOPT_ENCODING => '',
-                        CURLOPT_MAXREDIRS => 10,
-                        CURLOPT_TIMEOUT => 0,
-                        CURLOPT_FOLLOWLOCATION => TRUE,
-                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                        CURLOPT_CUSTOMREQUEST => 'POST',
+            $url = API_URL.''.VERSION.'/profiles/self/pages/'.PAGE_ID.'/records';
+            $param_arr=array(
                         CURLOPT_HTTPHEADER => array('Authorization: Bearer ' . $token),
                         CURLOPT_POST => true,
                         CURLOPT_POSTFIELDS => $request_body,
-                    )
-            );
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $data['response'] = json_decode($response, true);
+                    );
+            $data['response']= api_call($url,'POST',$param_arr);
             echo json_encode($data, true);
         } catch (Exception $e) {
             print_r($e->getMessage());
